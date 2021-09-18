7dfbfbe47d5c38a9b0983321aede03743f38b41d
Ana Beatriz Araujo de Sousa <anab.araujos@gmail.com>
2021-09-18 10:59:34 -0300

    quarta questao em java

---
+++ src/questoes_java/quarta-questao/aumentoSalario.java
@@ -0,0 +1,32 @@
+package quartaQuestaoParadigmas;
+
+public class aumentoSalario {
+	private String nome;
+	private double salario;
+	private double imposto;
+	private double aumento;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public aumentoSalario(String nome, double salario, double imposto) {
+		this.nome = nome;
+		this.salario = salario;
+		this.imposto = imposto;
+	}
+	
+	public double salarioLiquido(double salario, double imposto) {
+		this.salario -= this.imposto;
+		
+		return this.salario;
+	}
+	
+	public double aumento(double salario, double aumento) {
+		this.aumento = aumento;
+		this.salario = this.salario + this.salario * (this.aumento/100);
+		
+		return this.salario;
+	}
+}
